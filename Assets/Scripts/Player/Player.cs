311be227e67c5035ab9f3bdf75cbdaa250282651
Steven Juve <taskmasterjcodes@gmail.com>
2023-01-20 20:37:33 +0700

    Added HealthBar functionality to the player
    untuk TakeDamage() function di Player.cs bisa di set aja terserah.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,72 +4,92 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float playerSpeed;
-    public bool shield = false;
-    private Rigidbody2D rb;
-    public int hp = 20;
-    private float movementInputDirectionX, movementInputdirectionY;
-    public Animator anim;
-    public float dashSpeed = 50f;
-    public Vector3 moveDir;
-    private bool canDash = true;
-    //private float dashTime;
-    //private float dashCoolDown = .1f;
-    private bool isDashing;
-    public float parryRange;
-    private Vector3 lastDir;
-    public GameObject ParryPoint;
-    public LayerMask projectileLayer;
-    private bool canParry = false;
-    // Start is called before the first frame update
-    void Start()
+  public float playerSpeed;
+  public HealthBar healthBar;
+  public int maxHealth = 100;
+  public int currentHealth;
+  public bool shield = false;
+  private Rigidbody2D rb;
+  public int hp = 20;
+  private float movementInputDirectionX, movementInputdirectionY;
+  public Animator anim;
+  public float dashSpeed = 50f;
+  public Vector3 moveDir;
+  private bool canDash = true;
+  //private float dashTime;
+  //private float dashCoolDown = .1f;
+  private bool isDashing;
+  public float parryRange;
+  private Vector3 lastDir;
+  public GameObject ParryPoint;
+  public LayerMask projectileLayer;
+  private bool canParry = false;
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    anim = GetComponent<Animator>();
+
+    // setting player health
+    currentHealth = maxHealth;
+    healthBar.SetMaxHealth(maxHealth);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyUp(KeyCode.E))
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+      shield = false;
+      canParry = false;
+      anim.SetBool("isShield", shield);
     }
 
-    // Update is called once per frame
-    void Update()
+    if (canParry && shield == false)
     {
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            shield = false;
-            canParry = false;
-            anim.SetBool("isShield", shield);
-        }
-
-        if (canParry && shield == false)
-        {
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                canParry = false;
-                Debug.Log("Parried");
-            }
-        }
+      if (Input.GetKeyDown(KeyCode.Q))
+      {
+        canParry = false;
+        Debug.Log("Parried");
+      }
     }
 
-    private void FixedUpdate()
-    {
-        movementInputDirectionX = Input.GetAxisRaw("Horizontal");
-        movementInputdirectionY = Input.GetAxisRaw("Vertical");
-        moveDir = new Vector2(movementInputDirectionX, movementInputdirectionY).normalized;
-        if (moveDir != new Vector3(0, 0, 0))
-            lastDir = moveDir;
-        else
-            lastDir =new  Vector3(transform.localScale.x, 0, 0);
-        rb.velocity = moveDir * playerSpeed *Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Mouse1) && canDash)
-        {
-            isDashing = true;
-            canDash = false;
-            Vector3 dashPosition = transform.position + lastDir * dashSpeed;
-            rb.MovePosition(dashPosition);
-            canDash = true;
-        }
+    // For debugging
+    // if (Input.GetKeyDown(KeyCode.Space))
+    // {
+    //   TakeDamage(20);
+    // }
+  }
 
-        if (movementInputDirectionX > 0)
-            transform.localScale = new Vector2(1, 1);
-        else if (movementInputDirectionX < 0)
-            transform.localScale = new Vector2(-1, 1);
+  private void FixedUpdate()
+  {
+    movementInputDirectionX = Input.GetAxisRaw("Horizontal");
+    movementInputdirectionY = Input.GetAxisRaw("Vertical");
+    moveDir = new Vector2(movementInputDirectionX, movementInputdirectionY).normalized;
+    if (moveDir != new Vector3(0, 0, 0))
+      lastDir = moveDir;
+    else
+      lastDir = new Vector3(transform.localScale.x, 0, 0);
+    rb.velocity = moveDir * playerSpeed * Time.deltaTime;
+    if (Input.GetKeyDown(KeyCode.Mouse1) && canDash)
+    {
+      isDashing = true;
+      canDash = false;
+      Vector3 dashPosition = transform.position + lastDir * dashSpeed;
+      rb.MovePosition(dashPosition);
+      canDash = true;
     }
+
+    if (movementInputDirectionX > 0)
+      transform.localScale = new Vector2(1, 1);
+    else if (movementInputDirectionX < 0)
+      transform.localScale = new Vector2(-1, 1);
+  }
+
+  // for taking damage
+  void TakeDamage(int damage)
+  {
+    currentHealth -= damage;
+    healthBar.SetHealth(currentHealth);
+  }
 }
