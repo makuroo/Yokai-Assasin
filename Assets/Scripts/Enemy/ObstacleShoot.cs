99ec2a426eb2e8d46028dcc9af4b4396abbf3384
makuroo <92146487+makuroo@users.noreply.github.com>
2023-08-06 20:13:08 +0700

    update bug

--- Assets/Scripts/Enemy/ObstacleShoot.cs
+++ Assets/Scripts/Enemy/ObstacleShoot.cs
@@ -14,43 +14,13 @@ public class ObstacleShoot : MonoBehaviour
 
     private const string ShootTrigger = "ShootTrigger";
 
-    private void Awake()
-    {
-        
-    }
-
-  [SerializeField] private ObstacleProjectile projectile;
-  [SerializeField] private Animator animator;
-  private int damage;
-  bool shoot = false;
-  [SerializeField] private float fireRate;
-
-  private const string ShootTrigger = "ShootTrigger";
-
-  private void Awake()
-  {
-    // animator = GetComponent<Animator>();
-  }
-
-  private void Update()
-  {
-    if (shoot == false)
-    {
-      StartCoroutine(Shoot(fireRate));
-    }
-  }
-
-  private IEnumerator Shoot(float fireRate)
-  {
-    shoot = true;
-    animator.SetTrigger(ShootTrigger);
-    yield return new WaitForSeconds(fireRate);
-    Instantiate(projectile, transform.position, transform.rotation);
-    yield return new WaitForSeconds(fireRate - 0.5f);
-
-    animator.ResetTrigger(ShootTrigger);
-    shoot = false;
-  }
+     private void Update()
+     {
+        if (shoot == false)
+        {
+          StartCoroutine(Shoot(fireRate));
+        }
+     }
 
     private IEnumerator Shoot(float fireRate)
     {
