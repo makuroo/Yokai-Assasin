9648938bd0af11079f65034c7bf4fcfe7ebad385
makuroo <92146487+makuroo@users.noreply.github.com>
2023-08-06 19:57:38 +0700

    update dummy shoot mechanic

--- Assets/Scripts/Enemy/ObstacleShoot.cs
+++ Assets/Scripts/Enemy/ObstacleShoot.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 
 public class ObstacleShoot : MonoBehaviour
 {
+    [SerializeField] private ObstacleProjectile projectile;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform[] shootPoint; 
+    private int damage;
+    bool shoot = false;
+    [SerializeField] private float fireRate;
+
+    private const string ShootTrigger = "ShootTrigger";
+
+    private void Awake()
+    {
+        
+    }
 
   [SerializeField] private ObstacleProjectile projectile;
   [SerializeField] private Animator animator;
@@ -39,5 +52,20 @@ public class ObstacleShoot : MonoBehaviour
     shoot = false;
   }
 
+    private IEnumerator Shoot(float fireRate)
+    {
+        shoot = true;
+        animator.SetTrigger(ShootTrigger);
+        
+        yield return new WaitForSeconds(fireRate);
+
+        animator.ResetTrigger(ShootTrigger);
+        shoot = false;
+    }
+
+    public void InstantiateProjectile() {
+        Instantiate(projectile, shootPoint[0].transform.position, shootPoint[0].rotation);
+        Instantiate(projectile, shootPoint[1].transform.position, shootPoint[1].rotation);
+    }
 
 }
