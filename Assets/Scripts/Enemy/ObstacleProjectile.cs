98e858b8305c026c28ef3f877c7d465726a32ec0
Steven Juve <taskmasterjcodes@gmail.com>
2023-08-07 18:33:59 +0700

    added OnCollision

--- Assets/Scripts/Enemy/ObstacleProjectile.cs
+++ Assets/Scripts/Enemy/ObstacleProjectile.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class ObstacleProjectile : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] int damageAmount = 1;
+    public Player player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            if (player != null)
+            {
+                player.TakeDamage(damageAmount);
+            }
+
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
